888375d3e9246404bf95924abc5d413ae4e41431
Roger Kiew <yuichiu416@gmail.com>
2019-06-27 16:13:49 -0500

    Fixed bugs at level-8

--- board.rb
+++ board.rb
@@ -14,7 +14,7 @@ class Board
       nums.map { |num| Tile.new(num) }
     end
 
-    self.new(tiles)
+    Board.new(tiles)
   end
 
   def initialize(grid = self.empty_grid)
@@ -52,10 +52,9 @@ class Board
   end
 
   def solved?
-    rows.all? { |row| solved_set?(row) } &&
-      columns.all? { |col| solved_set?(col) } &&
-      squares.all? { |square| solved_set?(square) }
-    end
+    rows.all? { |row| solved_set?(row) } && 
+    columns.all? { |col| solved_set?(col) } && 
+    squares.all? { |square| solved_set?(square) }
   end
 
   def solved_set?(tiles)
